7719259ec17dfa7aa371a5124fd14af1bc35ed2d
EshwaryForReasons <mishra.eshwary@gmail.com>
2023-10-27 02:44:10 -0700

    Previous commit failed to remove build directories, I have removed them now.

--- build/CMakeFiles/eshymail.dir/source/accounts.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/eshymail.dir/source/accounts.c.o: \
- /home/eshy/EshyMail/source/accounts.c /usr/include/stdc-predef.h
--- build/CMakeFiles/eshymail.dir/source/accounts.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/eshymail.dir/source/accounts.cpp.o: \
- /home/eshy/EshyMail/source/accounts.cpp /usr/include/stdc-predef.h
